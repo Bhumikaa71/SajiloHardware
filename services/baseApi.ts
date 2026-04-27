834d4a4ce6f254b824e9cf211023fb745eaad879
Bhumika <baumabista@gmail.com>
2026-04-27 16:12:35 +0545

    adding readmoreblog page

--- services/baseApi.ts
+++ services/baseApi.ts
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
   reducerPath: 'api', // The key in your Redux store
   baseQuery: fetchBaseQuery({ 
-    // baseUrl: process.env.NEXT_PUBLIC_API_URL || 'https://sajilo-hardware.onrender.com',
-    baseUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
+    baseUrl: process.env.NEXT_PUBLIC_API_URL || 'https://sajilo-hardware.onrender.com',
+    // baseUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
     prepareHeaders: (headers) => {
       const sh_token = localStorage.getItem('sh-token');
       if (sh_token) headers.set('authorization', `Bearer ${sh_token}`);
