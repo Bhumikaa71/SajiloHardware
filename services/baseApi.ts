82127db5bda45dad771afaa77bee0fa3ea8afd7f
Pushit <pushitcoc@gmail.com>
2026-05-12 17:21:06 +0545

    updated

--- services/baseApi.ts
+++ services/baseApi.ts
@@ -9,6 +9,7 @@ export const baseApi = createApi({
   refetchOnMountOrArgChange: false, // ✅ MUST be false - this is your skeleton cause
   baseQuery: fetchBaseQuery({
     // baseUrl: process.env.NEXT_PUBLIC_BASE_URL || 'https://sajilo-hardware.onrender.com', //baseUrl: 'https://sajilo-hardware.onrender.com
+    
     baseUrl: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000',
     // baseUrl: 'http://localhost:3000',
     prepareHeaders: (headers) => {
